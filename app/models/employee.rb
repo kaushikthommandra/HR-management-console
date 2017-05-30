ba84ce882a9ef825fd0f8c49d4b5a252c01a294f
aks <ankit.singhaniya@reportgarden.com>
2017-05-30 16:45:33 +0530

    add employee list for reference

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,17 @@
 class Employee < ApplicationRecord
+
+  # NOTE: we are overridding the default acitve record method here
+  ## we will want to remove this in the next session
+  ## the goal of this mehtod is to explain the absraction without the knowledge of model
+  def self.all
+    return [
+      {
+        name: 'Ashok Varma',
+        designation: 'CEO',
+        age: '45', # :P
+        date_of_joining: '01-04-2011'
+      },
+      # TODO: add more people here
+    ]
+  end
 end
